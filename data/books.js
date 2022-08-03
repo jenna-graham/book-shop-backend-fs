8ada6600245b4aa6a77c49c5b06e7b7aab9d6755
jenna-graham <jclayto2@gmail.com>
2022-08-03 09:44:23 -0700

    added books table

---
+++ data/books.js
@@ -0,0 +1,13 @@
+const books = [
+    { id: '1', title: 'A New Earth', released: '2005'},
+    { id: '2', title: 'The Book of Joy', released: '2016'},
+    { id: '3', title: 'The Alchemist', released: '1993'},
+    { id: '4', title: 'grokking algorithms', released: '2016'},
+    { id: '5', title: 'Sacred Woman', released: '2000'},
+    { id: '6', title: 'The Power of Now', released: '1997'},
+    { id: '7', title: 'Untamed', released: '2020'},
+    { id: '8', title: 'Melanin Empath', released: '2019'},
+    { id: '9', title: 'You Are The Universe', released: '2017'},
+];
+
+module.exports = { books };
