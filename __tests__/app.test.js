7d3ab5438b47803527e027511311c52f21c2febc
jenna-graham <jclayto2@gmail.com>
2022-08-03 10:47:06 -0700

    passed 'list of books' test, added controller and model for books/Book

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,10 +9,9 @@ describe('backend-express-template routes', () => {
   });
   it('GET /books should return a list of books', async () => {
     const res = await request(app).get('/books');
-    expect(res.body.length.toEqual(9));
+    expect(res.body.length).toEqual(9);
     const aNewEarth = res.body.find((char) => char.id === '1');
     expect(aNewEarth).toHaveProperty('title', 'A New Earth');
-    expect(aNewEarth).toHaveProperty('released', '2005');
   });
   afterAll(() => {
     pool.end();
