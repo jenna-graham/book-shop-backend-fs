e0cdc595672c155d52e6cddd7f15d76fd68112d3
jenna-graham <jclayto2@gmail.com>
2022-08-03 11:35:59 -0700

    get route by id test and controllers/models

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -13,6 +13,15 @@ describe('backend-express-template routes', () => {
     const aNewEarth = res.body.find((char) => char.id === '1');
     expect(aNewEarth).toHaveProperty('title', 'A New Earth');
   });
+  it('GET /books/:id should return the book detail', async () => {
+    const res = await request(app).get('/books/1');
+    expect(res.body).toEqual({
+      id: '1',
+      title: 'A New Earth',
+      released: 2005,
+    });
+  });
+
   afterAll(() => {
     pool.end();
   });
