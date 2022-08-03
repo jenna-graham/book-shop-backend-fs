d4bca83440def634b87078fd81e6d7fd9d1edefc
jenna-graham <jclayto2@gmail.com>
2022-08-03 10:06:33 -0700

    setup db the correct way this time! books

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,14 +1,19 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('GET /books should return a list of books', async () => {
+    const res = await request(app).get('/books');
+    expect(res.body.length.toEqual(9));
+    expect(res.body[0]).toEqual({
+      id: expect.any(String),
+      title: expect.any(String),
+    });
   });
   afterAll(() => {
     pool.end();
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,20 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP table if exists books;
+
+CREATE table books (
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    title VARCHAR NOT NULL,
+    released INT
+);
+
+INSERT INTO books (title, released) VALUES
+('A New Earth', '2005'),
+('The Book of Joy', '2016'),
+('The Alchemist', '1993'),
+('grokking algorithms', '2016'),
+('Sacred Woman', '2000'),
+('The Power of Now', '1997'),
+('Untamed', '2020'),
+('Melanin Empath', '2019'),
+('You Are The Universe', '2017');
